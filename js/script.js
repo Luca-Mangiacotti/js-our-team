77d4e3ed38f3bb0d3e9b8340b158d1daa26cca11
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2024-12-15 17:30:54 +0100

    feat add member by form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+//FUNCTION
+
+//funzione che crea l'html di una singola CARD
+function createNewCard (newMember) {
+  const {name, role, email, img} = newMember
+
+  const newCard = `<div class="TeamCard">
+                      <img src=${img} alt="${name}" class="img-card">
+                      <div class="CardDescription">
+                          <h3>${name}</h3>
+                          <p>${role}</p>
+                          <a href="#">${email}</a>
+                      </div>
+                    </div>`
+
+  return newCard
+}
+
+//Initial VARIABLES
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -39,20 +58,47 @@ const teamMembers = [
 
 // DOM ELEMENTS
 const TeamGridElm = document.querySelector(".TeamGrid")
+  //form elements
+  const formElm = document.getElementById("addMember")
+  const newMemNameElm = document.getElementById("newMemName")
+  const newMemRoleElm = document.getElementById("newMemRole")
+  const newMemEmailElm = document.getElementById("newMemEmail")
+  const newMemImgElm = document.getElementById("newMemImg")
+  const submitElm = document.getElementById("submitBtn")
 
+// STARTING PAGE
 //stampa delle card di default 
 let listMembers = ""
 for ( let i = 0; i < teamMembers.length; i++ ){
-  //destrutturazione
-  const {name, role, email, img} = teamMembers[i]
-  listMembers += `<div class="TeamCard">
-                    <img src=${img} alt="${name}" class="img-card">
-                    <div class="CardDescription">
-                      <h3>${name}</h3>
-                      <p>${role}</p>
-                      <a href="#">${email}</a>
-                    </div>
-                  </div>`
-
+  
+  listMembers += createNewCard(teamMembers[i])
 }
 TeamGridElm.innerHTML = listMembers
+
+//DOM EVENTS
+
+//form submit
+formElm.addEventListener("submit" , function(event){
+
+  event.preventDefault()
+  const name = newMemNameElm.value
+  const role = newMemRoleElm.value
+  const email = newMemEmailElm.value
+  const imgMem = newMemImgElm.value
+
+  const newMember = {
+      name,
+      role,
+      email,
+      imgMem
+  }
+
+  //aggiornamento dell' array teamMembers
+  teamMembers.push(newMember)
+
+  //creazione della nuova card da inserire
+  const currentCard = createNewCard(newMember)
+
+  //inserimento della card nella pagina HTML
+  TeamGridElm.innerHTML += currentCard
+})
